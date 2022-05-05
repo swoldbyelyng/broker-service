424e6593527f7ec4efc519ae7ba282f7d437d6a9
David Mikkelsen <s147197@student.dtu.dk>
2022-05-05 09:28:51 +0200

    Added classes = BrokerServiceApplication.class to the BrokerServiceApplicationTests - the test won't know what to test and will fail otherwise

--- src/test/java/com/broker/BrokerServiceApplicationTests.java
+++ src/test/java/com/broker/BrokerServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.broker;
 
+import com.broker.controller.BrokerServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BrokerServiceApplication.class)
 class BrokerServiceApplicationTests {
 
 	@Test
